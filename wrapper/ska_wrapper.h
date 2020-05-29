c81d48946a04728f24fde7f7fac07b40b7e7f782
Ubuntu <ubuntu@ip-172-31-41-137.ec2.internal>
2020-05-29 19:35:18 +0000

    added ska testing wrapper is still incorrect

---
+++ wrapper/ska_wrapper.h
@@ -0,0 +1,106 @@
+#ifndef SKA_WRAPPER
+#define SKA_WRAPPER
+
+#include "../data_structures/flat_hash_map.hpp"
+#include "wrapper/stupid_iterator.h"
+
+class SkaWrapper
+{
+private:
+    using HashType = flat_hash_map;
+
+    HashType hash;
+    size_t capacity;
+
+public:
+    using key_type           = size_t;
+    using mapped_type        = size_t;
+    using value_type         = typename std::pair<const key_type, mapped_type>;
+    using iterator           = StupidIterator<key_type, mapped_type>;
+    using insert_return_type = std::pair<iterator, bool>;
+
+    SkaWrapper() = delete;
+    SkaWrapper(size_t capacity_) : hash(capacity_), capacity(capacity_) {}
+    SkaWrapper(const SkaWrapper&) = delete;
+    SkaWrapper& operator=(const SkaWrapper&) = delete;
+
+    SkaWrapper(SkaWrapper&& rhs) : hash(rhs.capacity), capacity(rhs.capacity) {}
+
+    SkaWrapper& operator=(SkaWrapper&& rhs)
+    {
+        capacity = rhs.capacity;
+        (& hash)->~HashType();
+        new (& hash) HashType(rhs.capacity);
+        return *this;
+    }
+
+    using Handle = SkaWrapper&;
+    Handle get_handle() { return *this; }
+    
+    inline iterator find(const key_type& k)
+    {
+      bool found;
+      valtype v = hash.Get(k, found);
+      if(found) {
+        return iterator(k, v);
+      } else {
+        return end();
+      }
+    }
+
+    inline insert_return_type insert(const key_type &k, const mapped_type &d) {
+      bool inserted = hash.Insert(k, d);
+      return {{inserted ? k : 0, d}, inserted};
+    }
+
+    template<class F, class ... Types>
+    inline insert_return_type update(const key_type& k, F f, Types&& ... args)
+    {
+        //if (! F::junction_compatible::value) return insert_return_type(end(), false);
+
+        // extract the update value
+        auto update_value = mapped_type();
+        f(update_value, std::forward<Types>(args)...);
+
+        // update only
+        bool updated = hash.Update(k, update_value);
+        return {{updated ? k : 0, update_value}, updated};
+    }
+
+    template<class F, class ... Types>
+    inline insert_return_type insert_or_update(const key_type& k, const mapped_type& d, __attribute__((unused)) F f, __attribute__((unused)) Types&& ... args)
+    {
+        //if (! F::junction_compatible::value) return insert_return_type(end(), false);
+
+        // extract the update value
+        auto update_value = mapped_type();
+        f(update_value, std::forward<Types>(args)...);
+
+        // insert or update
+        auto inserted = hash.insert_or_update(k, d, update_value);
+
+        // construct the return value
+        if(inserted) return {iterator(k, d), true}; // inserted
+        else return {iterator(k, update_value), false}; // updated
+    }
+
+    template<class F, class ... Types>
+    inline insert_return_type update_unsafe(const key_type& k, F f, Types&& ... args)
+    {
+        return hash.update(k,f,std::forward<Types>(args)...);
+    }
+
+    template<class F, class ... Types>
+    inline insert_return_type insert_or_update_unsafe(const key_type& k, const mapped_type& d, F f, Types&& ... args)
+    {
+        return hash.insert_or_update(k,d,f,std::forward<Types>(args)...);
+    }
+
+    inline size_t erase(const key_type& k)
+    {
+        return hash.erase(k) ? 1ul : 0ul;
+    }
+
+    inline iterator end() { return iterator(); }
+};
+#endif // ROBINHOOD_WRAPPER
