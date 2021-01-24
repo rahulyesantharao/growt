4a082704d091c8bf88fc10c0e1dc55fb99b17263
Sai Sameer Pusapaty <pusapaty@mit.edu>
2021-01-23 20:57:57 -0500

    fixed tabs

--- build/graph_script.py
+++ build/graph_script.py
@@ -42,80 +42,80 @@ args = parser.parse_args()
 # Formatting metadata
 #####################
 if args.range_num_threads:
-	num = args.range_num_threads[0]
-	while num <= args.range_num_threads[1]:
-		THREAD_NUMS.append(num)
-		num *= 2
+        num = args.range_num_threads[0]
+        while num <= args.range_num_threads[1]:
+                THREAD_NUMS.append(num)
+                num *= 2
 elif args.list_num_threads:
-	THREAD_NUMS = args.list_num_threads
+        THREAD_NUMS = args.list_num_threads
 else:
-	print("NO THREAD NUMS SPECIFIED")
+        print("NO THREAD NUMS SPECIFIED")
 
 for benchmark in args.benchmarks:
-	DATA[benchmark] = {}
-	for col in BENCHMARK_TO_COLS[benchmark]:
-		DATA[benchmark][col] = {}
-		for ID in args.tables:
-			DATA[benchmark][col][ID_TO_TABLE[ID]] = {}
-			for num_threads in THREAD_NUMS:
-				DATA[benchmark][col][ID_TO_TABLE[ID]][num_threads] = []
+        DATA[benchmark] = {}
+        for col in BENCHMARK_TO_COLS[benchmark]:
+                DATA[benchmark][col] = {}
+                for ID in args.tables:
+                        DATA[benchmark][col][ID_TO_TABLE[ID]] = {}
+                        for num_threads in THREAD_NUMS:
+                                DATA[benchmark][col][ID_TO_TABLE[ID]][num_threads] = []
 
 ####################
 # Setting up scripts
 ####################
 print("Making scripts...")
 for benchmark in args.benchmarks:
-	# remove previous versions of the script
-	subprocess.run(["rm", ID_TO_BENCHMARK[benchmark]+".sh"])
-	with open(ID_TO_BENCHMARK[benchmark]+".sh", "a") as f:
-		for ID in args.tables:
-			table = ID_TO_TABLE[ID]
-			f.write("echo TABLE: {};\n".format(table))
-			for num_threads in THREAD_NUMS:
-				if benchmark == "i":
-					f.write(ins_benchmark_exec_cmd.format(table, args.num_elem,
-														  num_threads, args.iterations))
-				elif benchmark == "d":
-					f.write(del_benchmark_exec_cmd.format(table, args.num_elem,
-													  	  num_threads, args.iterations))
-				elif benchmark == "m":
-					f.write(mix_benchmark_exec_cmd.format(table, args.num_elem, args.capacity,
-													  	  args.stream_size, num_threads,
-													      args.iterations, args.wperc))
-				f.write("\n")
-	subprocess.run(["chmod", "+x", ID_TO_BENCHMARK[benchmark]+".sh"])
+        # remove previous versions of the script
+        subprocess.run(["rm", ID_TO_BENCHMARK[benchmark]+".sh"])
+        with open(ID_TO_BENCHMARK[benchmark]+".sh", "a") as f:
+                for ID in args.tables:
+                        table = ID_TO_TABLE[ID]
+                        f.write("echo TABLE: {};\n".format(table))
+                        for num_threads in THREAD_NUMS:
+                                if benchmark == "i":
+                                        f.write(ins_benchmark_exec_cmd.format(table, args.num_elem,
+                                                                                                                  num_threads, args.iterations))
+                                elif benchmark == "d":
+                                        f.write(del_benchmark_exec_cmd.format(table, args.num_elem,
+                                                                                                                  num_threads, args.iterations))
+                                elif benchmark == "m":
+                                        f.write(mix_benchmark_exec_cmd.format(table, args.num_elem, args.capacity,
+                                                                                                                  args.stream_size, num_threads,
+                                                                                                              args.iterations, args.wperc))
+                                f.write("\n")
+        subprocess.run(["chmod", "+x", ID_TO_BENCHMARK[benchmark]+".sh"])
 
 ####################
 # Running benchmarks
 ####################
 print("Running scripts...")
 for benchmark in args.benchmarks:
-	os.system("./"+ID_TO_BENCHMARK[benchmark]+".sh > "+ID_TO_BENCHMARK[benchmark]+".out")
+        os.system("./"+ID_TO_BENCHMARK[benchmark]+".sh > "+ID_TO_BENCHMARK[benchmark]+".out")
 
 ###################
 # Parsing outputs
 ###################
 def get_data(raw_line, header):
-	data = {}
-	line = raw_line.split()
-	for i in range(len(line)):
-		data[header[i]] = line[i]
-	return data
+        data = {}
+        line = raw_line.split()
+        for i in range(len(line)):
+                data[header[i]] = line[i]
+        return data
 
 print("Parsing output...")
 for benchmark in args.benchmarks:
-	with open(ID_TO_BENCHMARK[benchmark]+".out") as f:
-		current_table = ""
-		current_header = []
-		for line in f:
-			if line.startswith("TABLE"):
-				current_table = line.split()[1]
-			elif line.startswith(" #i"):
-				current_header = line.split()
+        with open(ID_TO_BENCHMARK[benchmark]+".out") as f:
+                current_table = ""
+                current_header = []
+                for line in f:
+                        if line.startswith("TABLE"):
+                                current_table = line.split()[1]
+                        elif line.startswith(" #i"):
+                                current_header = line.split()
                         elif len(current_header) == len(line.split()):
-				data = get_data(line, current_header)
-				for col in BENCHMARK_TO_COLS[benchmark]:
-					DATA[benchmark][col][current_table][int(data["p"])].append(float(data[col]))
+                                data = get_data(line, current_header)
+                                for col in BENCHMARK_TO_COLS[benchmark]:
+                                        DATA[benchmark][col][current_table][int(data["p"])].append(float(data[col]))
 
 ###################
 # Graphing Results
@@ -124,19 +124,19 @@ print("Graphing output...")
 x_vars = THREAD_NUMS
 Y_VARS = {}
 for benchmark in args.benchmarks:
-	Y_VARS[benchmark] = {}
-	for col in BENCHMARK_TO_COLS[benchmark]:
-		Y_VARS[benchmark][col] = {}
-		for ID in args.tables:
-			table = ID_TO_TABLE[ID]
-			Y_VARS[benchmark][col][table] = []
-			for p in THREAD_NUMS:
-				all_trials = DATA[benchmark][col][current_table][p]
-				val = np.array(all_trials).mean()
-				Y_VARS[benchmark][col][table].append(val)
+        Y_VARS[benchmark] = {}
+        for col in BENCHMARK_TO_COLS[benchmark]:
+                Y_VARS[benchmark][col] = {}
+                for ID in args.tables:
+                        table = ID_TO_TABLE[ID]
+                        Y_VARS[benchmark][col][table] = []
+                        for p in THREAD_NUMS:
+                                all_trials = DATA[benchmark][col][current_table][p]
+                                val = np.array(all_trials).mean()
+                                Y_VARS[benchmark][col][table].append(val)
 
 for ID in args.tables:
-	plt.plot(x_vars, Y_VARS["i"]["t_ins"][ID_TO_TABLE[ID]])
+        plt.plot(x_vars, Y_VARS["i"]["t_ins"][ID_TO_TABLE[ID]])
 #plt.savefig('foo.png')
 
 
